6e25c746bc2d6de19f88f14135ec0b3dbf8a71b4
ceske-42 <ceske@etik.com>
2025-03-04 16:04:29 +0100

    zio

---
+++ prova.c
@@ -0,0 +1,7 @@
+#include "LIBFT/headers/libft.h"
+
+int main(int ac, char **av)
+{
+	char **a = ft_split("12", '\0');
+	ft_isdigit(a[0][0]);
+}
--- push_swap.c
+++ push_swap.c
@@ -1,11 +1,72 @@
 #include "LIBFT/headers/libft.h"
 
+int fill_stack(const char ***list, long int **stack, int is_allocd)
+{
+	int			i;
+	int			j;
+	int			n;
+	
+	i = 0;
+	n = 0;
+	while(list[i])
+	{
+		j = 0;
+		while(list[i][j])
+		{
+			if(is_allocd)
+			{
+				(*stack)[n] = ft_atoi(list[i][j]);
+				//ft_printf("stack[%i] = %i\n", n, (*stack)[n]);
+			}
+			j++;
+			n++;
+		}
+		i++;
+	}
+	if(!is_allocd)
+	{
+		*stack = malloc(n * sizeof(int));
+		if(!stack)
+			return 0;		
+		fill_stack(list, stack, 1);
+	}
+	return 1;
+}
+
 int main(int ac, char **av)
 {
-	int i;
+	int			i;
+	int			j;
+	long int	*stack;
+	char		***list;
 
 	i = 0;
-	while()
+	while(av[++i])
+	{
+		j = 0;
+		while(av[i][j])
+		{
+			if(!(ft_isdigit(av[i][j]) || ft_isspace(av[i][j])))
+			{
+				ft_printf("Error");
+				exit(EXIT_FAILURE);
+			}
+			j++;
+		}
+	}
 
-	ft_printf("%i", ft_atoi("a"));
+	list = malloc((ac) * sizeof(char**));
+	if(!list)
+		exit(EXIT_FAILURE);
+	i = 0;
+	while(av[++i])
+		list[i - 1] = ft_split(av[i], ' ');
+	if(!fill_stack((const char ***)list, &stack, 0))
+		exit(EXIT_FAILURE);
+	i = 0;
+	while(stack[i])
+	{
+		ft_printf("stack[%i] = %i\n", i, stack[i]);
+		i++;
+	}
 }
