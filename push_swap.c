009fbe0a854b21b4c7397460292788b9dd04f6a8
Riccardo Emanuele Todaro <rtodaro@e4r10p2.42roma.it>
2025-03-05 16:33:06 +0100

    fixed: get_stack_as_a_string_array

--- push_swap.c
+++ push_swap.c
@@ -1,10 +1,48 @@
 #include "LIBFT/headers/libft.h"
 #include <stdio.h>
+#include <unistd.h>
+
+//da eliminare 
+static long     ft_atol(char *str)
+{
+        long    num;
+        int     sign;
+
+        num = 0;
+        sign = 1;
+        if (*str == '-')
+        {
+                sign = -sign;
+                str++;
+        }
+        while (*str)
+        {
+                num *= 10;
+                num += (*str - '0');
+                str++;
+        }
+        return (num * sign);
+}
+//da eliminare
+
 char **get_stack_as_a_string_array(char **args)
 {
 	int			i;
 	char		**string_stack;
 
+	string_stack = NULL;
+	
+	string_stack = (char **)malloc(sizeof(char *));
+	if (!string_stack)
+		exit(EXIT_FAILURE);
+	
+	*string_stack = (char *)malloc(sizeof(1));
+	if (!*string_stack)
+	{
+		free(string_stack);
+		exit(EXIT_FAILURE);
+	}
+	
 	*string_stack = "\0";
 	i = 0;
 	while(args[++i])
@@ -32,7 +70,6 @@ long int *get_num_stack(char **string_stack)
 	long int	*stack;
 
 	stack = malloc(ft_strlen((char *)string_stack) * sizeof(long int));
-	stack = malloc(i * sizeof(long int));
 	i = 0;
 	while(string_stack[i])
 	{
@@ -55,27 +92,8 @@ int main(int ac, char **av)
 	char		**string_stack;
 	long int	*stack;
 
-	// string_stack = get_stack_as_a_string_array(av);
-
-	*string_stack = "\0";
-	i = 0;
-	while(av[++i])
-	{
-		*string_stack = ft_strjoin(*string_stack, av[i]);
-		*string_stack = ft_strjoin(*string_stack, " ");
-	}
-	i = 0;
-	while((*string_stack)[i])
-	{
-		if(!(ft_isdigit((*string_stack)[i]) || ft_isspace((*string_stack)[i])))
-		{
-			ft_printf("Error");
-			exit(EXIT_FAILURE);
-		}
-		i++;
-	}
-	string_stack = ft_split(*string_stack, ' ');
-
+	string_stack = get_stack_as_a_string_array(av);
+	
 	i = 0;
 	stack = get_num_stack(string_stack);
 	i = 0;
