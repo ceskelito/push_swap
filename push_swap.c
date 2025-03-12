493aad75b5d85ffa91ab0b4cdb2928002c7ba01b
ceske-42 <ceske@etik.com>
2025-03-12 15:31:44 +0100

    Devo liberare la memoria

--- push_swap.c
+++ push_swap.c
@@ -6,17 +6,23 @@
 /*   By: rceschel <rceschel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 13:08:20 by rceschel          #+#    #+#             */
-/*   Updated: 2025/03/12 13:08:42 by rceschel         ###   ########.fr       */
+/*   Updated: 2025/03/12 15:30:36 by rceschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-char **get_stack_as_a_string_array(char **args)
+void	exit_error(void)
 {
-	int			i;
-	char		**string_stack;
-	
+	ft_printf("Error");
+	exit(EXIT_FAILURE);
+}
+
+char	**get_stack_as_a_string_array(char **args)
+{
+	int		i;
+	char	**string_stack;
+
 	string_stack = (char **)malloc(sizeof(char *));
 	if (!string_stack)
 		exit(EXIT_FAILURE);
@@ -28,64 +34,59 @@ char **get_stack_as_a_string_array(char **args)
 	}
 	*string_stack = "\0";
 	i = 0;
-	while(args[++i])
-	{
-		*string_stack = ft_strjoin(*string_stack, args[i]);
-		*string_stack = ft_strjoin(*string_stack, " ");
-	}
+	while (args[++i])
+		*string_stack = ft_strjoin_multi(3, *string_stack, args[i], " ");
 	i = 0;
-	while((*string_stack)[i])
+	while ((*string_stack)[i])
 	{
-		if(!(ft_isdigit((*string_stack)[i]) || ft_isspace((*string_stack)[i])))
-		{
-			ft_printf("Error");
-			exit(EXIT_FAILURE);
-		}
+		if (!(ft_isdigit((*string_stack)[i]) || ft_isspace((*string_stack)[i])))
+			exit_error();
 		i++;
 	}
-	return(ft_split(*string_stack, ' '));
+	return (ft_split(*string_stack, ' '));
 }
 
-long int *get_num_stack(char **string_stack)
+t_stack_single	get_stack(char **string_stack)
 {
-	int		i;
-	int		j;
-	long 	*stack;
+	int				i;
+	int				j;
+	t_stack_single	stack;
 
-	stack = malloc(ft_strlen((char *)string_stack) * sizeof(long int));
+	stack.list = malloc(ft_strlen((char *)string_stack) * sizeof(long int));
 	i = 0;
-	while(string_stack[i])
+	while (string_stack[i])
 	{
-		stack[i] = ft_atol(string_stack[i]);
+		stack.list[i] = ft_atol(string_stack[i]);
 		j = i;
-		while(j-- >= 0)
-			if((i != j && stack[i] == stack[j]) || stack[i] != (int)stack[i])
-			{
-				ft_printf("Error");
-				exit(EXIT_FAILURE);
-			}
+		if (stack.list[i] != (int)stack.list[i])
+			exit_error();
+		while (--j >= 0)
+			if (stack.list[i] == stack.list[j])
+				exit_error();
 		i++;
 	}
+	stack.size = i;
+	stack.capacity = i;
 	return (stack);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	int				i;
-	char			**string_stack;
-	static t_stack	stack;
+	int						i;
+	char					**string_stack;
+	static t_stack_compose	stack;
 
-	if(ac <= 1)
+	if (ac <= 1)
 		exit(EXIT_SUCCESS);
 	string_stack = get_stack_as_a_string_array(av);
 	i = 0;
-	stack.a = get_num_stack(string_stack);
-	stack.b = ft_calloc(1, sizeof(stack.a));
+	stack.a = get_stack(string_stack);
+	stack.b.list = ft_calloc(1, sizeof(stack.a));
 	i = 0;
-	while(stack.a[i])
+	while (stack.a.list[i])
 	{
-		ft_printf("stack_a[%i] = %i\n", i, stack.a[i]);
-		ft_printf("stack_b[%i] = %i\n", i, stack.b[i]);
+		ft_printf("stack_a[%i] = %i\t\t\t", i, stack.a.list[i]);
+		ft_printf("stack_b[%i] = %i\n", i, stack.b.list[i]);
 		i++;
 	}
 }
--- stack_struct.h
+++ stack_struct.h
@@ -6,42 +6,24 @@
 /*   By: rceschel <rceschel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 13:06:29 by rceschel          #+#    #+#             */
-/*   Updated: 2025/03/12 13:11:47 by rceschel         ###   ########.fr       */
+/*   Updated: 2025/03/12 14:24:50 by rceschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STACK_STRUCT_H
 # define STACK_STRUCT_H
 
-typedef struct s_stack_info
+typedef struct s_stack_single
 {
-	long				*arr;
+	long				*list;
 	int					size;
 	int					capacity;
-}						t_stack_info;
+}						t_stack_single;
 
-typedef struct s_stack
+typedef struct s_stack_compose
 {
-	union
-	{
-		t_stack_info	stack_a;
-		struct
-		{
-			long		*a;
-			int			a_size;
-			int			a_capacity;
-		};
-	};
-	union
-	{
-		t_stack_info	stack_b;
-		struct
-		{
-			long		*b;
-			int			b_size;
-			int			b_capacity;
-		};
-	};
-}						t_stack;
+	t_stack_single	a;
+	t_stack_single	b;
+}					t_stack_compose;
 
 #endif
